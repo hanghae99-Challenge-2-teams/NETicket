075046cd30998ea051769e4249eafa99e9dfde6e
jangjinhyeok <jangjh45@naver.com>
2023-03-31 20:54:50 +0900

    Feat : 스켈레톤 코드 생성
    스켈레톤 코드 작성 완료

---
+++ src/main/java/com/example/neticket/NeTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.neticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeTicketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NeTicketApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/neticket/NeTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeTicketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
