c08f74235d11261258d5e09bfb1bd6efe880fb40
ChoonB <shgkdc@gmail.com>
2023-04-03 22:14:19 +0900

    Fix : isAvailable TicketInfoResponseDTO에 추가
    isAvailable TicketInfoResponseDTO에 추가
    
    Related to: #15

--- src/main/java/com/example/neticket/event/dto/TicketInfoResponseDto.java
+++ src/main/java/com/example/neticket/event/dto/TicketInfoResponseDto.java
@@ -10,11 +10,13 @@ public class TicketInfoResponseDto {
   private Long ticketInfoId;
   private LocalDateTime openDate;
   private int leftSeats;
+  private boolean isAvailable;
 
 
   public TicketInfoResponseDto(TicketInfo ticketInfo) {
     this.ticketInfoId = ticketInfo.getId();
     this.openDate = ticketInfo.getOpenDate();
     this.leftSeats = ticketInfo.getTotalSeats() - ticketInfo.getReservedSeats();
+    this.isAvailable = ticketInfo.isAvailable();
   }
 }
