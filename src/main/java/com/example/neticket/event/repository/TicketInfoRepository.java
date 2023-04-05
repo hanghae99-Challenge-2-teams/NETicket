8d87442c16bbbaf08bda51df57c3d009a9bce7d2
jangjinhyeok <jangjh45@naver.com>
2023-04-05 21:15:18 +0900

    fix : MySql RDS로 바꾸고 LockAcquisitionException 오류 해결
    TicketInfo를 pessimistic lock으로 바꿔서 LockAcquisitionException 해결
    
    Resolved : #15

--- src/main/java/com/example/neticket/event/repository/TicketInfoRepository.java
+++ src/main/java/com/example/neticket/event/repository/TicketInfoRepository.java
@@ -1,8 +1,16 @@
 package com.example.neticket.event.repository;
 
 import com.example.neticket.event.entity.TicketInfo;
+import io.lettuce.core.dynamic.annotation.Param;
+import java.util.Optional;
+import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TicketInfoRepository extends JpaRepository<TicketInfo, Long> {
 
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @Query("SELECT t FROM TicketInfo t WHERE t.id = :id")
+  Optional<TicketInfo> findByIdWithLock(@Param("id") Long id);
 }
