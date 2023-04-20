85cf356fe681885974e0738461fc696d7cbebb3c
ChoonB <shgkdc@gmail.com>
2023-04-20 10:35:09 +0900

    Feat : 서버응답시간 측정 aop 구현
    서버 응답시간 측정 aop 구현
    admin 컨트롤러 생성해서 reservation 컨트롤러 분리
    html에 응답시간 받아주는 함수 생성
    
    Related to: #73

--- src/main/java/com/example/neticket/reservation/service/ReservationService.java
+++ src/main/java/com/example/neticket/reservation/service/ReservationService.java
@@ -133,45 +133,6 @@ public class ReservationService {
     );
   }
 
-  //  5. ADMIN. DB에서 남은 좌석수만 가져와서 Redis에 (key-value)형태로 저장. 이미 저장되어있으면 예외처리
-  @Transactional
-  public MessageResponseDto saveLeftSeatsInRedis(Long ticketInfoId, User user) {
-    checkAdmin(user);
-    TicketInfo ticketInfo = checkTicketInfoById(ticketInfoId);
-    redisRepository.saveTicketInfoToRedis(ticketInfo);
-    return new MessageResponseDto(HttpStatus.CREATED, "redis에 성공적으로 저장되었습니다.");
-  }
-
-  //  5-1. user role이 admin인지 체크
-  private void checkAdmin(User user) {
-    if (!user.getRole().equals(UserRoleEnum.ADMIN)) {
-      throw new CustomException(ExceptionType.USER_UNAUTHORIZED_EXCEPTION);
-    }
-  }
-
-  //  6. ADMIN. 해당하는 공연의 남은 좌석수 Redis에서 삭제(삭제되기전 모든 캐시 DB에 반영)
-  @Transactional
-  public MessageResponseDto deleteLeftSeatsFromRedis(Long ticketInfoId, User user) {
-    checkAdmin(user);
-    redisRepository.deleteLeftSeatsInRedis(ticketInfoId);
-    return new MessageResponseDto(HttpStatus.OK, "redis에서 캐시가 성공적으로 삭제되었습니다.");
-  }
-
-//  7. ADMIN. 현재 Redis에 등록된 모든 LeftSeats의 key를 리스트로 반환
-  @Transactional(readOnly = true)
-  public List<String> findAllLeftSeatsKeysInRedis(User user) {
-    checkAdmin(user);
-    return new ArrayList<>(redisRepository.findAllLeftSeatsKeysInRedis());
-  }
-
-//  8. ADMIN. Redis와 DB의 LeftSeats를 정확한 값으로 Refresh
-  @Transactional
-  public MessageResponseDto refreshLeftSeats(Long ticketInfoId, User user) {
-    checkAdmin(user);
-    redisRepository.refreshLeftSeats(ticketInfoId);
-    return new MessageResponseDto(HttpStatus.OK, "redis와 DB의 남은 좌석수를 올바르게 맞췄습니다.");
-  }
-
 
 }
 
