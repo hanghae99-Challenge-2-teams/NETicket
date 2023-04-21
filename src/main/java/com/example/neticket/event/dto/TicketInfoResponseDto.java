6ffc018c3ffef8327d2a4442162b8604dabff41e
jangjinhyeok <jangjh45@naver.com>
2023-04-21 10:07:20 +0900

    Refactor : DetailEventResponseDto에 필요없는Lombok 어노테이션 제거
    DetailEventResponseDto.java
    
    Resolved : # 75

--- src/main/java/com/example/neticket/event/dto/TicketInfoResponseDto.java
+++ src/main/java/com/example/neticket/event/dto/TicketInfoResponseDto.java
@@ -2,29 +2,26 @@ package com.example.neticket.event.dto;
 
 import com.example.neticket.event.entity.TicketInfo;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Builder
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class TicketInfoResponseDto {
 
   private Long ticketInfoId;
   private LocalDateTime openDate;
-//  private int leftSeats;
   private boolean isAvailable;
 
+  // DetailEventResponseDto 역직렬화 할때 사용함
+  @Builder
+  public TicketInfoResponseDto() {
+  }
 
   public TicketInfoResponseDto(TicketInfo ticketInfo) {
     this.ticketInfoId = ticketInfo.getId();
     this.openDate = ticketInfo.getOpenDate();
-//    this.leftSeats = ticketInfo.getLeftSeats();
     this.isAvailable = ticketInfo.isAvailable();
   }
 }
--- src/main/java/com/example/neticket/reservation/service/ReservationService.java
+++ src/main/java/com/example/neticket/reservation/service/ReservationService.java
@@ -16,8 +16,6 @@ import java.time.Duration;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
