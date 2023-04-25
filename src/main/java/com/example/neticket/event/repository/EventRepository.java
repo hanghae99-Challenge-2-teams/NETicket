1d1d1551a17f88fb40b27e55507b0682bcbb5fbe
ChoonB <shgkdc@gmail.com>
2023-04-25 12:12:06 +0900

    Feat : 공연 정보 추가, 검색 수정
    공연정보 50개까지 추가
    검색+메인 쿼리 수정
    localdatetime 프론트에서 보여지는 값 수정
    페이징 함수 검색용으로 새로 추가
    
    Related to: #82

--- src/main/java/com/example/neticket/event/repository/EventRepository.java
+++ src/main/java/com/example/neticket/event/repository/EventRepository.java
@@ -10,11 +10,11 @@ import org.springframework.data.repository.query.Param;
 public interface EventRepository extends JpaRepository<Event, Long> {
 
 //  Event가 가진 ticketInfo에서 isAvailable이 true인 것을 가져오고 날짜가 가장 빠른순으로 정렬해서 page로 반환
-  @Query("SELECT e FROM Event e JOIN e.ticketInfo t WHERE t.isAvailable = true GROUP BY e.id ORDER BY MIN(e.date) ASC")
+  @Query("SELECT e FROM Event e JOIN e.ticketInfo t WHERE t.isAvailable = true GROUP BY e.id ORDER BY e.date ASC")
   Page<Event> findAllByAvailableOrderByticketInfoDate(Pageable pageable);
 
 //  keyword로 Event의 title과 place에서 검색해 page로 반환. 예매가능한게 먼저 뜨고 가장 최근에 등록된 공연 순으로 정렬된다.
-  @Query("SELECT e FROM Event e JOIN e.ticketInfo t WHERE (e.title LIKE %:keyword% OR e.place LIKE %:keyword%) ORDER BY t.isAvailable DESC, e.id DESC")
+  @Query("SELECT e FROM Event e JOIN e.ticketInfo t WHERE (e.title LIKE %:keyword% OR e.place LIKE %:keyword%) ORDER BY t.isAvailable DESC, e.date ASC")
   Page<Event> findAllByTitleOrPlaceContainingAndAvailableTickets(@Param("keyword") String keyword, Pageable pageable);
 
 }
--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -136,16 +136,17 @@ function createCard(id, image, title, date, place, available) {
   imageContainer.onerror = function () {
     imageContainer.src = 'path/to/default/image.jpg'; // 대체 이미지 경로로 수정
   };
+  let newDate = dateFormat(date);
 
   let descriptionContainer = document.createElement('div');
   descriptionContainer.classList.add('property-description');
   // descriptionContainer.innerHTML = `<h5>${title}</h5><h6>${date}</h6><p>${place}</p><p>${available}</p>`;
   if (available === null) {
-    descriptionContainer.innerHTML = `<h5>${title}</h5><h6>${date}</h6><p>${place}</p>`;
+    descriptionContainer.innerHTML = `<h5>${title}</h5><h6>${newDate}</h6><p>${place}</p>`;
   } else if (available) {
-    descriptionContainer.innerHTML = `<h5>${title}</h5><h6>${date}</h6><p>${place}</p><p style="color:green">예매 가능</p>`;
+    descriptionContainer.innerHTML = `<h5>${title}</h5><h6>${newDate}</h6><p>${place}</p><p style="color:green">예매 가능</p>`;
   } else {
-    descriptionContainer.innerHTML = `<h5>${title}</h5><h6>${date}</h6><p>${place}</p><p style="color:red">예매 불가능</p>`;
+    descriptionContainer.innerHTML = `<h5>${title}</h5><h6>${newDate}</h6><p>${place}</p><p style="color:red">예매 불가능</p>`;
   }
 
   let socialIconsContainer = document.createElement('div');
@@ -259,16 +260,18 @@ function getEventDetails(eventId) {
     url: "/api/neticket/events/" + eventId,
     dataType: "json",
     success: function (event) {
+      let newEventDate = dateFormat(event.date);
+      let newOpenDate = dateFormat(event.ticketInfoDto.openDate);
       const imageUrl = 'https://neticketbucket.s3.ap-northeast-2.amazonaws.com/uploaded-image/'
           + event.image;
       $('.image').attr('src', imageUrl);
       $('.title').text(event.title);
       $('.place').text(event.place);
       $('.price').text(addCommas(event.price) + '원');
-      $('.date').text(event.date);
+      $('.date').text(newEventDate);
       isAvailable = event.ticketInfoDto.available;
       $('.isAvailable').text(isAvailable ? '예매 가능' : '예매 불가능');
-      $('.remainingTime1').text(event.ticketInfoDto.openDate);
+      $('.remainingTime1').text(newOpenDate);
       // 이벤트의 티켓 정보를 저장합니다.
       $('#bookingButton').data('ticketInfo', event.ticketInfoDto);
       // 남은 시간을 표시합니다.
@@ -347,13 +350,14 @@ function getEventInfo() {
     url: "/api/neticket/ticket-info/" + ticketInfoId,
     dataType: "json",
     success: function (event) {
+      let newDate = dateFormat(event.date);
       const imageUrl = 'https://neticketbucket.s3.ap-northeast-2.amazonaws.com/uploaded-image/'
           + event.image;
       $('.image').attr('src', imageUrl);
       $('.title').text(event.title);
       $('.place').text(event.place);
       $('.price').text(addCommas(event.price) + '원');
-      $('.date').text(event.date);
+      $('.date').text(newDate);
     },
     error: function (jqXHR, textStatus, errorThrown) {
       alert("요청이 실패하였습니다.");
@@ -425,13 +429,14 @@ function showReservationCompleted() {
       'Authorization': token // Authorization 헤더에 토큰 값 추가
     },
     success: function (response) {
+      let newDate = dateFormat(response.date);
       const imageUrl = 'https://neticketbucket.s3.ap-northeast-2.amazonaws.com/uploaded-image/'
           + response.image;
       $('.image').attr('src', imageUrl);
       $('.id').text(response.id)
       $('.title').text(response.title)
       $('.place').text(response.place);
-      $('.date').text(response.date);
+      $('.date').text(newDate);
       $('.totalPrice').text(response.totalPrice);
       $('.count').text(response.count);
     },
@@ -640,6 +645,7 @@ function showMyPage() {
         let date = responseArray[i].date;
         let totalPrice = responseArray[i].totalPrice;
         let count = responseArray[i].count;
+        let newDate = dateFormat(date);
 
         let temp = `
                     <ul class="info">
@@ -653,7 +659,7 @@ function showMyPage() {
                         <div class="infoDesc"><p class="infoText">${place}</p></div>
                       </li>
                       <li class="infoItem infoDate"><strong class="infoLabel">공연 날짜 : </strong>
-                        <div class="infoDesc"><p class="infoText">${date}</p></div>
+                        <div class="infoDesc"><p class="infoText">${newDate}</p></div>
                       </li>
                       <li class="infoItem infoPrice"><strong class="infoLabel">총 가격 : </strong>
                         <div class="infoDesc"><p class="infoText">${totalPrice}원</p></div>
@@ -712,10 +718,9 @@ function showMyPage() {
 }
 
 // 검색 페이지
-function showSearchResult() {
+function showSearchResult(pageNum) {
   const urlParams = new URLSearchParams(window.location.search);
   const keyword = urlParams.get('keyword');
-  let pageNum = urlParams.get('page');
   if (pageNum === null) {
     pageNum = 1;
   }
@@ -728,8 +733,7 @@ function showSearchResult() {
     dataType: "json",
     headers: {'Content-Type': 'application/json'},
     success: function (response) {
-      showPaging(response);
-      console.log(response)
+      showSearchPaging(response);
       if (response.content.length === 0) {
         alert("키워드에 맞는 공연 정보가 없습니다.");
       }
@@ -767,4 +771,58 @@ function showSearchResult() {
       alert(response.responseJSON.msg);
     }
   });
+}
+
+function showSearchPaging(response) {
+  let totalPages = response.totalPages;
+  let currentPage = response.number + 1;
+  let pagination = $('.pagination');
+
+  pagination.empty();
+
+//let startPage = Math.floor((currentPage - 1) / 10) * 10 + 1;
+  let startPage = Math.floor((currentPage - 1) / 2) * 2 + 1; // 페이지 2개로 실험
+  // let endPage = startPage + 9;
+  let endPage = startPage + 1;
+  if (endPage > totalPages) {
+    endPage = totalPages;
+  }
+
+  if (startPage > 1) {
+    pagination.append(`<li class="page-item">
+                          <a class="page-link" onclick="showSearchResult(${startPage
+    - 1})" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                          </a>
+                        </li>`);
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    let pageItem = `<li class="page-item ${(i === currentPage) ? 'active' : ''}">
+                      <a class="page-link" onclick="showSearchResult(${i})" href="#">${i}</a>
+                    </li>`;
+    pagination.append(pageItem);
+  }
+
+  if (endPage < totalPages) {
+    pagination.append(`<li class="page-item">
+                          <a class="page-link" onclick="showSearchResult(${endPage + 1})" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                          </a>
+                        </li>`);
+  }
+}
+
+function dateFormat(date) {
+  const datetime = new Date(date);
+
+  const year = datetime.getFullYear();
+  const month = ('0' + (datetime.getMonth() + 1)).slice(-2);
+  const day = ('0' + datetime.getDate()).slice(-2);
+  const hours = ('0' + datetime.getHours()).slice(-2);
+  const minutes = ('0' + datetime.getMinutes()).slice(-2);
+
+  const formattedDatetimeStr = year + '-' + month + '-' + day + ' ' + hours + '시 ' + minutes + '분';
+  return formattedDatetimeStr;
+
 }
