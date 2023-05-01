7d09552d213a1102914ca7772ec6986a12cbde57
jangjinhyeok <jangjh45@naver.com>
2023-05-01 11:51:08 +0900

    Test : Cacheble제거 Test
    user캐시 제거 후 테스트
    
    Resolved : #60

--- src/main/java/com/example/neticket/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/neticket/security/UserDetailsServiceImpl.java
@@ -21,13 +21,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
   //  토큰에 있는 email로 유저 정보 조회
   @Override
-  @Cacheable(cacheNames = "localCache", key = "#email")
+//  @Cacheable(cacheNames = "localCache", key = "#email")
   public UserDetails loadUserByUsername(String email) {
     User user = userRepository.findByEmail(email).orElseThrow(
         () -> new CustomException(ExceptionType.NOT_FOUND_USER_EXCEPTION)
     );
 //    로그 추후 삭제
-    log.info("UserDetails DB로 조회중");
+//    log.info("UserDetails DB로 조회중");
     return new UserDetailsImpl(user, user.getEmail());
 
   }
