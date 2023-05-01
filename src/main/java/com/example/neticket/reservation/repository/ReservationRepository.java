313bb3bb3ac5d5db20bac8019bbf28227a7bebb8
ChoonB <shgkdc@gmail.com>
2023-05-01 09:57:40 +0900

    Feat: 리셋 버튼 로직 변경
    reservation 기록 다 지우고 남은 좌석수 총 좌석수로 맞추기

--- src/main/java/com/example/neticket/reservation/repository/ReservationRepository.java
+++ src/main/java/com/example/neticket/reservation/repository/ReservationRepository.java
@@ -11,6 +11,8 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
   List<Reservation> findAllByUserOrderByIdDesc(User user);
 
+  void deleteAll();
+
   // 실제 예매된 좌석 수 반환
   @Query("SELECT SUM(r.count) FROM Reservation r WHERE r.ticketInfoId = :ticketInfoId")
   Integer sumCountByTicketInfoId(@Param("ticketInfoId") Long ticketInfoId);
